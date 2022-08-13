beae160a8602a66a579a3f92771190a5e081b828
YutoUrushima <yuto259233@gmail.com>
2022-08-13 23:17:59 +0900

    Display username

--- lib/session.php
+++ lib/session.php
@@ -1,15 +1,22 @@
 <?php
+include_once __DIR__ . '/db.php';
 session_start();
 
 class Session
 {
     public function create_current_user($user_id)
     {
+        $pdo = new ConnectDB();
+        $sql = 'SELECT name, email FROM users WHERE id = ' . $user_id . ';';
+        $user_info = $pdo->select($sql);
+        $user_alias = $user_info[0]['name'] ? $user_info[0]['name'] : $user_info[0]['email'];
+        $_SESSION['current_user_alias'] = $user_alias;
         $_SESSION['current_user'] = $user_id;
     }
     public function delete_current_user()
     {
         unset($_SESSION['current_user']);
+        unset($_SESSION['current_user_alias']);
     }
     public function validate_current_user()
     {
@@ -20,5 +27,9 @@ class Session
     public function check_current_user()
     {
         return isset($_SESSION['current_user']);
+    }
+    public function get_user_alias()
+    {
+        return $_SESSION['current_user_alias'] ? $_SESSION['current_user_alias'] : null;
     }
 }
--- partial/head.php
+++ partial/head.php
@@ -11,20 +11,27 @@ $session = new Session();
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
 </head>
 <body>
-    <header class="d-flex justify-content-around d-flex align-items-center bg-light">
-        <h1 class="p-3"><a href="/" class="text-decoration-none text-dark">Rhino</a></h1>
-        <nav>
-            <ul class="list-unstyled d-flex fs-5">
-                <li class="me-2"><a href="/user" class="text-decoration-none">user</a></li>
-                <li><a href="/article" class="text-decoration-none">artilce</a></li>
-            </ul>
-        </nav>
+    <header class="d-flex justify-content-between d-flex align-items-center bg-light px-5">
         <div>
+            <h1 class="p-3"><a href="/" class="text-decoration-none text-dark">Rhino</a></h1>
+        </div>
+        <div class="d-flex align-items-center">
             <?php if ($session->check_current_user()) { ?>
-                <a class="btn btn-primary" href="<?php __DIR__; ?>/system/logout" role="button">Log out</a>
-            <?php } else { ?>
-                <a class="btn btn-primary" href="/signin" role="button">Sign in</a>
+                <nav>
+                    <ul class="list-unstyled d-flex fs-5 mb-0 ms-3">
+                        <li class="me-2">
+                            <a href="/user" class="btn btn-outline-primary"><?php echo $session->get_user_alias(); ?></a>
+                        </li>
+                    </ul>
+                </nav>
             <?php } ?>
+            <div class="ms-3">
+                <?php if ($session->check_current_user()) { ?>
+                    <a class="btn btn-primary" href="<?php __DIR__; ?>/system/logout" role="button">Log out</a>
+                <?php } else { ?>
+                    <a class="btn btn-primary" href="/signin" role="button">Sign in</a>
+                <?php } ?>
+            </div>
         </div>
     </header>
-    <div class="container py-3">
+    <div class="container py-5">
